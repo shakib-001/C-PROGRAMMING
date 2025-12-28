0f012ce83b799e324bc658d6c4a79b3e14ff8e7b
shakib-001 <138766692+shakib-001@users.noreply.github.com>
2025-12-28 20:15:41 +0600

    Add files via upload

---
+++ 4.functions/1basic.cpp
@@ -0,0 +1,100 @@
+/*#include<stdio.h>
+
+void shakib(){
+    printf("Hi Shakib\n");
+}
+
+int main(){
+    shakib();
+    shakib();
+    shakib();
+    return 0;
+} */
+/*
+#include<stdio.h>
+int shakib(){
+    return 959;
+}
+int main(){
+    int a=shakib();
+    printf("%d",a);
+    return 0;
+}  */
+/*#include<stdio.h>
+int shakib(int a){
+    return a+9;
+}
+int main(){
+    int n=9;
+    printf("%d",shakib(n));
+}  */
+
+/*#include<stdio.h>
+int shakib(int a){
+    return a+9;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",shakib(n));
+}   */
+
+/*#include<stdio.h>
+int shakib(int a, int b){
+    return a+b;
+}
+int main(){
+    int n=9;
+    int m=5;
+    printf("%d",shakib(n,m));
+}   */
+
+/*
+#include<stdio.h>
+int shakib(int a,int b){
+    int sum= a+b;
+    return sum;
+}
+int main(){
+    int m;
+    scanf("%d",&m);
+    int n;
+    scanf("%d",&n);
+    printf("The sum is: %d",shakib(m,n));
+}  */
+//factiriol number
+/*#include<stdio.h>
+
+int fact(int x){
+    int fact=1;
+    for(int i=1;i<=x;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d",fact(a));
+} */
+/*
+#include<stdio.h>
+
+int fact(int x){
+    int fact=1;
+    for(int i=1;i<=x;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        printf("%d\n",fact(i));
+    }
+    return 0;
+}  */
+
+
---
+++ 4.functions/2advance.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+sum(int a){
+   // a++;
+    return a;
+}
+int main(){
+    printf("%d",sum(8));
+    return 0;
+}
